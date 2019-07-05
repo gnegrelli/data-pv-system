0f577f6d010b570ad6b9b4d4314d8390423ad1b9
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2019-07-05 16:34:48 -0300

    Drop lines with junk data
    Lines with variable units and other unuseful info will not be included to dataframes

--- DataHandler.py
+++ DataHandler.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
-inverter = pd.read_csv("LACO_CSV_Relatório_mensal_2019_06.csv", sep=",")
+inverter = pd.read_csv("LACO_CSV_Relatório_mensal_2019_06.csv", sep=",", skiprows=[1])
 
-dlogger = pd.read_csv("CR300Series_Teste1.dat", sep=",", header=1)
+dlogger = pd.read_csv("CR300Series_Teste1.dat", sep=",", header=1, skiprows=[2, 3])
+
+print(inverter.head())
