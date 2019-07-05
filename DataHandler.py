2da2c2ac405f17d7d05771c72d53686b076934be
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2019-07-05 16:25:22 -0300

    Use second line of datalogger data as header

--- DataHandler.py
+++ DataHandler.py
@@ -2,6 +2,4 @@ import pandas as pd
 
 inverter = pd.read_csv("LACO_CSV_Relatório_mensal_2019_06.csv", sep=",")
 
-dlogger = pd.read_csv("CR300Series_Teste1.dat", sep=",")
-
-print(dlogger.describe())
+dlogger = pd.read_csv("CR300Series_Teste1.dat", sep=",", header=1)
