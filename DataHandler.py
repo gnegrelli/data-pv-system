ecf33c6c4b4f19a01ce812ba34807ad4763310d4
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2019-07-10 15:29:59 -0300

    Convert timestamps from files to standard

--- DataHandler.py
+++ DataHandler.py
@@ -5,3 +5,8 @@ inverter = pd.read_csv("LACO_CSV_Relatório_mensal_2019_06.csv", sep=",", skipro
 dlogger = pd.read_csv("CR300Series_Teste1.dat", sep=",", header=1, skiprows=[2, 3])
 
 inverter.drop(columns=['Unnamed: 14'], inplace=True)
+
+
+inverter['Timestamp'] = pd.to_datetime(inverter['Data e horário'], infer_datetime_format=True)
+
+dlogger['Timestamp'] = pd.to_datetime(dlogger['TIMESTAMP'], infer_datetime_format=True)
