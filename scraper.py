8f1c59087df074d6445315e969ca92192c706e0a
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2019-10-14 19:51:15 -0300

    Change start and end dates of query

--- scraper.py
+++ scraper.py
@@ -6,3 +6,11 @@ url = "http://www.inmet.gov.br/sonabra/pg_dspDadosCodigo_sim.php?QTcxMQ=="
 
 driver = webdriver.Firefox()
 driver.get(url)
+
+ini_date = driver.find_element_by_name('dtaini')
+ini_date.clear()
+ini_date.send_keys('30/08/2019')
+
+fin_date = driver.find_element_by_name('dtafim')
+fin_date.clear()
+fin_date.send_keys('01/10/2019')
