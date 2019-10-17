d6e08ad736fc1f3bf62f2d9ad38f20042cc57ca0
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2019-10-17 18:27:38 -0300

    Fill captcha value field

--- scraper.py
+++ scraper.py
@@ -14,3 +14,7 @@ ini_date.send_keys('30/08/2019')
 fin_date = driver.find_element_by_name('dtafim')
 fin_date.clear()
 fin_date.send_keys('01/10/2019')
+
+re_captcha = driver.find_element_by_name('aleaNum')
+re_captcha.clear()
+re_captcha.send_keys('2618')
